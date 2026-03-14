87cae558024cb3684296f61142c09451a3610802
WLFfair <zaditz12345setiawan@gmail.com>
2026-03-14 11:46:09 +0700

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,17 @@
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open('wolf-fair-v1').then(cache => {
+      return cache.addAll([
+        './',
+        './index.html',
+        './manifest.webmanifest'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
+});
